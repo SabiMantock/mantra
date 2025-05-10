79d83cd0dd13e4401355c275b0584b57dbe1859e
SabiMantock <sabimantock@gmail.com>
2025-05-10 20:49:19 +0100

    feat: implement transaction form with validation and input handling

--- frontend/src/components/form/Form.tsx
+++ frontend/src/components/form/Form.tsx
@@ -1,124 +1,19 @@
-import { Controller, useForm } from 'react-hook-form';
-import { FormData, FormProps } from '../../lib/types/types';
-import Button from '../ui/Button';
-import Input from '../ui/Input';
-import { categories } from '../../data/data';
-import { useEffect } from 'react';
+// import { Controller, useForm } from 'react-hook-form';
+// import { FormData, FormProps } from '../../lib/types/types';
+// import Button from '../ui/Button';
+// import Input from '../ui/Input';
+// import { categories } from '../../data/data';
+// import { useEffect } from 'react';
 
-const Form = ({ className, isExpense }: FormProps) => {
-  const {
-    control,
-    handleSubmit,
-    setValue,
-    formState: { errors:{
-      amount,
-      description,
-      date,
-      category,
-      repeat
-    } },
-  } = useForm<FormData>();
+const Form = () => {
 
-  useEffect(() => {
-    setValue('type', isExpense ? 'expense' : 'income');
-  }, [isExpense, setValue]);
 
-  
 
-  console.log({amount, description, date, category,repeat});
   return (
     <form
-      className={`${className} text-[#F8F8FA]`}
-      onSubmit={handleSubmit((data) => {
-        if (data.type === 'income' || data.type === 'expense') {
-          console.log(data);
-        } else {
-          console.error('Invalid type. Type must be either income or expense.');
-        }
-      })}
+
     >
-      <Controller
-        control={control}
-        name="amount"
-        rules={{
-          required: "This is required",
-          min: { value: 0, message: "Must be positive" },
-          pattern: {
-            value: /^\d*\.?\d+$/,
-            message: "Only positive numbers/decimals allowed",
-          },
-        }}
-        render={({ field: { onChange, value } }) => (
-          <Input
-            label="Amount"
-            type="number"
-            placeholder="0.00"
-            onChange={onChange}
-            value={value}
-            error={amount ? { message: amount.message || "" } : undefined}
-          />
-        )}
-      />
-      <Controller
-        control={control}
-        name="category"
-        rules={{ required: true }}
-        render={({ field: { onChange, value } }) => (
-          <Input
-            label="Category"
-            type="select"
-            placeholder="Select a category"
-            onChange={onChange}
-            categories={categories}
-            value={value}
-          />
-        )}
-      />
-      <Controller
-        control={control}
-        name="description"
-        rules={{ required: true }}
-        render={({ field: { onChange, value } }) => (
-          <Input
-            label="Description"
-            type="text"
-            placeholder={`What's this transaction for?`}
-            onChange={onChange}
-            value={value}
-          />
-        )}
-      />
-      <Controller
-        control={control}
-        rules={{ required: true }}
-        name="date"
-        render={({ field: { onChange, value } }) => (
-          <Input label="Date" type="date" onChange={onChange} value={value} />
-        )}
-      />
-      <Controller
-        control={control}
-        name="repeat"
-        // rules={{ required: true }}
-        render={({ field: { onChange, value } }) => (
-          <Input
-            label="Repeat"
-            type="checkbox"
-            onChange={onChange}
-            value={value}
-          />
-        )}
-      />
-      <div className="flex gap-2 items-center justify-end">
-        <Button
-          label={'Cancel'}
-          className="px-4 py-2 text-sm font-medium rounded-lg border cursor-pointer"
-        />
-        <Button
-          label={'Save Transaction'}
-          className={`px-4 py-2 text-sm font-medium rounded-lg border cursor-pointer ${isExpense ? 'bg-red-500 text-white' : 'bg-green-500 text-white'}`}
-        />
-      </div>
+
     </form>
   );
 };
--- frontend/src/components/transactions/TransactionForm.tsx
+++ frontend/src/components/transactions/TransactionForm.tsx
@@ -1,10 +1,32 @@
-import { useState } from 'react';
-import Form from '../form/Form';
+import { useEffect, useState } from 'react';
 import Button from '../ui/Button';
 import Card from '../ui/Card';
+import { Controller, useForm} from 'react-hook-form';
+import Input from '../ui/Input';
+import { FormData, FormProps } from '../../lib/types/types';
+import { categories } from '../../data/data';
 
-const TransactionForm = () => {
+const TransactionForm = ({ className }: FormProps) => {
   const [isExpense, setIsExpense] = useState(true);
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    formState: { errors:{
+      amount,
+      description,
+      date,
+      category,
+      repeat
+    } },
+  } = useForm<FormData>();
+
+    useEffect(() => {
+      setValue('type', isExpense ? 'expense' : 'income');
+    }, [isExpense, setValue]);
+
+
+  console.log({amount, description, date, category,repeat});
 
   return (
     <Card className="max-w-2xl mx-2 sm:mx-0 p-4 rounded-lg dark:bg-gray-800">
@@ -23,7 +45,97 @@ const TransactionForm = () => {
           onClick={() => setIsExpense(false)}
         />
       </div>
-      <Form isExpense={isExpense} />
+      <form       className={`${className} text-[#F8F8FA]`}
+      onSubmit={handleSubmit((data) => {
+        if (data.type === 'income' || data.type === 'expense') {
+          console.log(data);
+        } else {
+          console.error('Invalid type. Type must be either income or expense.');
+        }
+      })}>
+            <Controller
+        control={control}
+        name="amount"
+        rules={{
+          required: "This is required",
+          min: { value: 0, message: "Must be positive" },
+          pattern: {
+            value: /^\d*\.?\d+$/,
+            message: "Only positive numbers/decimals allowed",
+          },
+        }}
+        render={({ field: { onChange, value } }) => (
+          <Input
+            label="Amount"
+            type="number"
+            placeholder="0.00"
+            onChange={onChange}
+            value={value}
+            error={amount ? { message: amount.message || "" } : undefined}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="category"
+        rules={{ required: true }}
+        render={({ field: { onChange, value } }) => (
+          <Input
+            label="Category"
+            type="select"
+            placeholder="Select a category"
+            onChange={onChange}
+            categories={categories}
+            value={value}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="description"
+        rules={{ required: true }}
+        render={({ field: { onChange, value } }) => (
+          <Input
+            label="Description"
+            type="text"
+            placeholder={`What's this transaction for?`}
+            onChange={onChange}
+            value={value}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        rules={{ required: true }}
+        name="date"
+        render={({ field: { onChange, value } }) => (
+          <Input label="Date" type="date" onChange={onChange} value={value} />
+        )}
+      />
+      <Controller
+        control={control}
+        name="repeat"
+        // rules={{ required: true }}
+        render={({ field: { onChange, value } }) => (
+          <Input
+            label="Repeat"
+            type="checkbox"
+            onChange={onChange}
+            value={value}
+          />
+        )}
+      />
+      <div className="flex gap-2 items-center justify-end">
+        <Button
+          label={'Cancel'}
+          className="px-4 py-2 text-sm font-medium rounded-lg border cursor-pointer"
+        />
+        <Button
+          label={'Save Transaction'}
+          className={`px-4 py-2 text-sm font-medium rounded-lg border cursor-pointer ${isExpense ? 'bg-red-500 text-white' : 'bg-green-500 text-white'}`}
+        />
+      </div>
+      </form>
     </Card>
   );
 };
